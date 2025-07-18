e7cd1c35791db97b02d3fcd8089abc503ab01388
DIPTIMAN CHATTOPADHYAY <manikmondal34943@gmail.com>
2025-07-18 13:10:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ load_dotenv()
 # Telegram API
 API_ID = int(os.getenv('API_ID', 24925181))
 API_HASH = os.getenv('API_HASH', '88ec6570f92434a9db2000d1e78364e9')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '7246099288:AAFY9Rwq2Hhql4gAG7cN-J53qNERaN4HP2g')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8033915874:AAEGOEwc66mH3fNhdYg1s4z6N370hiTcxtc')
 
 # OTP Settings
-DEFAULT_2FA_PASSWORD = os.getenv('DEFAULT_2FA_PASSWORD', '112233')
+DEFAULT_2FA_PASSWORD = os.getenv('DEFAULT_2FA_PASSWORD', 'AA@@')
 
 # Database
 MONGO_URI = os.getenv('MONGO_URI', "mongodb+srv://noob:K3a4ofLngiMG8Hl9@tele.fjm9acq.mongodb.net/?retryWrites=true&w=majority")
 
 # Channels
-REQUESTED_CHANNEL = os.getenv('REQUESTED_CHANNEL', "-1002555911826")
-WITHDRAWAL_LOG_CHAT_ID = int(os.getenv('WITHDRAWAL_LOG_CHAT_ID', -1002626888395))
+REQUESTED_CHANNEL = os.getenv('REQUESTED_CHANNEL', "-1002119088215")
+WITHDRAWAL_LOG_CHAT_ID = int(os.getenv('WITHDRAWAL_LOG_CHAT_ID', -1002538505443))
 SEND_SESSION_CHANNEL_ID = int(os.getenv('SEND_SESSION_CHANNEL_ID', -1002533345170))
-ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '1211362365').split(',')]
+ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '1324269327').split(',')]
 
 # Directories
 SESSIONS_DIR = os.getenv('SESSIONS_DIR', "sessions")
